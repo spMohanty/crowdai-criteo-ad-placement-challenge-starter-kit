64573cf06bb59b928f07326c8c7b2f9a1b180943
SP Mohanty <sharada.mohanty@epfl.ch>
2017-10-31 07:22:03 +0100

    Add a random prediction generator; and update data

---
+++ generate_random_prediction.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from criteo_dataset import CriteoDataset
+import numpy as np
+
+GOLD_LABEL_PATH = "data/cntk_small.txt"
+
+data = CriteoDataset(GOLD_LABEL_PATH)
+
+for _impression in data:
+    num_of_candidates = len(_impression["candidates"])
+    predictions = np.random.rand(num_of_candidates)*10
+    predictions = ["{}:{}".format(idx, p) for idx, p in enumerate(predictions)]
+    predictionline = "{};{}".format(_impression["id"], ",".join(predictions))
+    print(predictionline)
